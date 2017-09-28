7fca21e096e550ccd070169f9f365794f0ea13ec
Ziwei <ziwei.hu@mail.utoronto.ca>
2017-09-28 10:18:29 -0400

    addded exception to kill ththe process when quitting

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,4 +2,10 @@ from bottle import route, run
 @route('/')
 def hello():
 	return "Hello World"
-run(host='localhost', port=8080, debug=True)
+	
+if __name__ == "__main__":
+
+	try:
+		run(host='localhost', port=8080, debug=True)
+	except Exception,ex:
+		print ex
