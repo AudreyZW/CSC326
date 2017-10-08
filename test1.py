adaff9e595276d3bdcddf2368569a44a54e6ea91
khancoder123 <farwakhan015@gmail.com>
2017-10-08 18:33:16 -0400

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,8 +1,20 @@
-from bottle import route, run,redirect
+from bottle import route, run, redirect
 
 @route('/')
 
 def home():
-    redirect("http://localhost:8080/linker")
+    #redirect("http://localhost:8090/")
+    return '<html><body>This is a very simple test.<a href= "http://localhost:8080/link">Linker page</a></body></html>'
+
+@route('/link')
+def link():
+    redirect('/linker')
+
+@route('/linker')
+def linker():
+    return '<html><body>This is what links to the first page.</body></html>'
+
+
 run(host='localhost', port=8080, debug=True)
+
 
