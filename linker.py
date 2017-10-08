c52cd1f2b60b519a4b874cbc65911bc30c7cec1a
khancoder123 <farwakhan015@gmail.com>
2017-10-08 19:39:10 -0400

    Delete linker.py

--- linker.py
+++
@@ -1,9 +0,0 @@
-from bottle import route, run
-
-@route('/linker')
-
-def linker():
-    return '<html><body>This is what links to the first page.</body></html>'
-
-run(host='localhost', port=8080, debug=True)
-
