ba0f4197c568502cb9ce63fb6927ce975cba5805
khancoder123 <farwakhan015@gmail.com>
2017-11-05 17:07:55 -0500

    Update integration_frontend.py

--- integration_frontend.py
+++ integration_frontend.py
@@ -1,4 +1,9 @@
+ 
     #integration with frontend
+    
+    bot = crawler(None, "urls.txt")
+    bot.crawl(depth=1)
+    
     scores_list = bot.get_docid_score() #get a dictionary of the scores as the value and the doc_id as the key
     sorted_scores = sorted(scores_list.items(), key=operator.itemgetter(1), reverse=True) #sort the dictionary in descending order by value
     list_of_url_ids = [score[0] for score in sorted_scores]#get only the doc_id
